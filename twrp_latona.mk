defbb3115d6329daab49353390a680ee7c2f69aa
MrSuperBuddy <128631074+MrSuperBuddy@users.noreply.github.com>
2024-04-23 10:40:59 +0300

    Create twrp_latona.mk

---
+++ twrp_latona.mk
@@ -0,0 +1,22 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from star device
+$(call inherit-product, GT-I9003/samsung/latona/device.mk)
+
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := latona
+PRODUCT_NAME := twrp_latona
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := GT-I9003
+PRODUCT_MANUFACTURER := samsung
